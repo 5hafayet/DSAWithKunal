b844aa49564457507f8017500b46595b1a18cbe2
5hafayet <shafayet.ahmad1@gmail.com>
2022-02-06 19:13:13 +0530

    new commit

--- Assignments/8-first-java/src/com/switchst/palindrome.java
+++ Assignments/8-first-java/src/com/switchst/palindrome.java
@@ -5,18 +5,8 @@ import java.util.Scanner;
 public class palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Type a String to check if it's Palindrome: ");
-        String hello = input.next();
-        int i = 0, j = hello.length()-1;
-        while(i > j ){
-            if(hello.charAt(i) == hello.charAt(j))
-                continue;
-            else {
-                System.out.println("Not Palindrome");
-            }
-            i++;
-            j--;
-        }
+        System.out.print("Type Number to check Armstrong or Not: ");
+        
 
 
 
