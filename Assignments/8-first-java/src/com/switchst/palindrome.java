9576f8dd7afe51da0d6b18f8f162148dacd9f235
5hafayet <shafayet.ahmad1@gmail.com>
2022-02-06 19:37:54 +0530

    first-java assignment complete

--- Assignments/8-first-java/src/com/switchst/palindrome.java
+++ Assignments/8-first-java/src/com/switchst/palindrome.java
@@ -6,7 +6,19 @@ public class palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Type Number to check Armstrong or Not: ");
-        
+        int number = input.nextInt();
+        int sum = 0;
+        int check = number;
+        while (number != 0) {
+            int temp = number % 10;
+            sum = sum + (temp * temp * temp);
+            number /= 10;
+        }
+
+        if(check == sum)
+            System.out.println("Armstrong.");
+        else
+            System.out.println("Not Armstrong.");
 
 
 
